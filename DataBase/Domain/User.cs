775d4d45c74357443322ae77f2bbfe6883f8dede
huihuiteresa <huihui_teresa@163.com>
2020-08-25 13:38:50 +0800

    aa

---
+++ DataBase/Domain/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataBase.Domain
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required,StringLength(30)]
+        public string Name { get; set; }
+
+
+        public int Age { get; set; }
+
+    }
+}
